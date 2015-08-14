da0aaf4eb47ea7dd496ef339dd91ba7820f3f210
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2015-08-14 16:33:41 +0300

    Правки страница виджетов подсказки

--- app/includes/models/admin/menu/TPFieldWidgets.php
+++ app/includes/models/admin/menu/TPFieldWidgets.php
@@ -10,7 +10,7 @@ class TPFieldWidgets {
     public function TPFieldWidget_1(){
         $widgets = 1;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
             <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('map-wiget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
@@ -20,18 +20,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
-
-            </label>
-        </div>
         <div class="TP-HeadTable TP-HeadTableCheckbox">
             <input id="chek1" type="checkbox" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets;?>][direct]"
                    value="1" <?php checked(isset(TPPlugin::$options['widgets'][$widgets]['direct']), 1) ?> hidden />
@@ -65,9 +55,9 @@ class TPFieldWidgets {
     public function TPFieldWidget_2(){
         $widgets = 2;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
-            <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('hotel-wiget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
+            <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('hotel-widget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
             <div class="svg-img-3">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 15 15"><g fill="#00B0DD">
@@ -75,17 +65,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-            </label>
-        </div>
         <div class="TP-HeadTable TPCheckBoxWidget">
             <input id="chek3" type="checkbox" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets;?>][draggable]"
                    value="1" <?php checked(isset(TPPlugin::$options['widgets'][$widgets]['draggable']), 1) ?> hidden />
@@ -170,7 +151,7 @@ class TPFieldWidgets {
     public function TPFieldWidget_3(){
         $widgets = 3;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
             <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('calendar-wiget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
@@ -180,17 +161,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-            </label>
-        </div>
         <div class="TP-ListSub ListSub--cust list--db">
 
             <div class="ItemSub ItemSub-1">
@@ -316,9 +288,10 @@ class TPFieldWidgets {
     public function TPFieldWidget_4(){
         $widgets = 4;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
-            <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('subscribe-wiget.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
+            <span class="TP-WidgetHelpImgSubsc">
+                <img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('subscribe-wiget.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
             <div class="svg-img-3">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 15 15"><g fill="#00B0DD">
@@ -326,17 +299,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-            </label>
-        </div>
         <div class="TP-ListSub ListSub--cust list--db">
 
             <div class="ItemSub ItemSub-1">
@@ -389,7 +353,7 @@ class TPFieldWidgets {
     public function TPFieldWidget_5(){
         $widgets = 5;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
             <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('one-hotel-wiget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
@@ -399,17 +363,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-            </label>
-        </div>
         <div class="TP-ListSub ListSub--cust list--db">
             <span class="TP-titleSub--custom"><?php _e('Width', KPDPlUGIN_TEXTDOMAIN ); ?> (px)</span>
             <div class="ItemSub ItemSub-3">
@@ -431,7 +386,7 @@ class TPFieldWidgets {
     public function TPFieldWidget_6(){
         $widgets = 6;
         ?>
-        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
+
         <a href="#" class="tooltip-img">
             <span><img src="<?php echo KPDPlUGIN_URL; ?>app/public/images/<?php _e('popular-destination-wiget-eng.png', KPDPlUGIN_TEXTDOMAIN) ?>" alt="" height="300px"/></span>
             <?php _e('Example widget', KPDPlUGIN_TEXTDOMAIN ); ?>
@@ -441,17 +396,8 @@ class TPFieldWidgets {
                         <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
             </div>
         </a>
-        <div class="TP-HeadTable">
-            <label>
-                <span><?php _e('Extra marker', KPDPlUGIN_TEXTDOMAIN ); ?></span>
-                <input type="text" name="<?php echo KPDPlUGIN_OPTION_NAME;?>[widgets][<?php echo $widgets; ?>][extra_widget_marker]"
-                       value="<?php echo esc_attr(TPPlugin::$options['widgets'][$widgets]['extra_widget_marker']) ?>"
-                       class="TPFieldInputText"/>
-            </label>
-            <label>
+        <a href="#" class="tooltip-img-2"><?php _e('Help Travepayouts', KPDPlUGIN_TEXTDOMAIN ); ?></a>
 
-            </label>
-        </div>
         <div class="TP-ListSub ListSub--cust list--db">
             <span class="TP-titleSub--custom"><?php _e('Width', KPDPlUGIN_TEXTDOMAIN ); ?> (px)</span>
             <div class="ItemSub  ItemSub-3">
