6081033abb86f623eb7a578ee4b2087d2b0628c6
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2015-09-09 16:22:38 +0300

    проблема с созданием второй папки плагина

--- app/includes/TPPlugin.php
+++ app/includes/TPPlugin.php
@@ -13,6 +13,9 @@ class TPPlugin extends \core\TPOPlugin implements \core\TPOPluginInterface{
         if( ! get_option(TPOPlUGIN_OPTION_NAME) )
             update_option( TPOPlUGIN_OPTION_NAME, TPDefault::defaultOptions() );
         models\admin\menu\TPSearchFormsModel::createTable();
+        //error_log(WP_PLUGIN_DIR);
+        //$sdirs = scandir(WP_PLUGIN_DIR);
+        //error_log(print_r($sdirs, true));
     }
 
     static public function deactivation()
