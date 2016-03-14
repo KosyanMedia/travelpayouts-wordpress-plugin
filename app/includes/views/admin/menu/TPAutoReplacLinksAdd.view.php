7e21d6476906b7b67f0c4a2a0ee4c607aa886cc2
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2016-03-14 12:19:44 +0200

    подсказка на Событие onclick "Можете добавить события Google Analytics или Yandex Metrika, которые будут вызываться при клике на ссылку" , "You can add here custom events (e.g. Google Analytics events) that will be fired after the click on a link"

--- app/includes/views/admin/menu/TPAutoReplacLinksAdd.view.php
+++ app/includes/views/admin/menu/TPAutoReplacLinksAdd.view.php
@@ -39,6 +39,14 @@
                 <label class="TP-LabelEvent">
                    <span>
                         <?php _e('Events onclick', TPOPlUGIN_TEXTDOMAIN) ?>
+                       <a href="#" class="tooltip-settings">
+                           <span><?php _e('You can add here custom events (e.g. Google Analytics events) that will be fired after the click on a link', TPOPlUGIN_TEXTDOMAIN); ?></span>
+                           <div class="svg-img-3">
+                               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 1 15 15"><g fill="#00B0DD">
+                                       <path d="M7.3 11.6c-.3 0-.5.2-.5.5v.4c0 .3.2.5.5.5s.5-.2.5-.5v-.4c.1-.2-.2-.5-.5-.5z"/>
+                                       <path d="M7.5 16c4.1 0 7.5-3.4 7.5-7.5S11.6 1 7.5 1 0 4.4 0 8.5 3.4 16 7.5 16zm0-13.9c3.5 0 6.4 2.9 6.4 6.4s-2.9 6.4-6.4 6.4S1.1 12 1.1 8.5 4 2.1 7.5 2.1z"/><path d="M5.2 7.2c.3 0 .5-.2.5-.5 0 0 0-.4.2-.9.3-.6.8-.8 1.5-.8.6 0 1.1.2 1.4.5.2.3.3.7.2 1.1-.1.5-.6 1-1 1.4-.6.6-1.2 1.2-1.2 1.9 0 .3.2.5.5.5s.5-.2.5-.5.4-.7.8-1.1c.6-.5 1.2-1.1 1.4-1.9.2-.7.1-1.5-.4-2-.3-.4-1-1-2.3-1-1.3 0-2 .8-2.3 1.4s-.4 1.3-.4 1.3c0 .3.3.6.6.6z"/></g></svg>
+                           </div>
+                       </a>
                    </span>
                    <textarea  name="arl_event"></textarea>
                 </label>
