c3592575742d767e43e366a2c999142b8af11a25
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2016-08-08 16:10:28 +0300

    Контроллер шорткода спецпредложения

---
+++ app/includes/controllers/site/shortcodes/TPSpecialOfferShortcodeController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeman
+ * Date: 08.08.16
+ * Time: 16:08
+ */
+
+namespace app\includes\controllers\site\shortcodes;
+
+
+class TPSpecialOfferShortcodeController extends \app\includes\controllers\site\TPShortcodesController
+{
+    public $model;
+    public $view;
+
+    public function initShortcode()
+    {
+        // TODO: Implement initShortcode() method.
+    }
+}
