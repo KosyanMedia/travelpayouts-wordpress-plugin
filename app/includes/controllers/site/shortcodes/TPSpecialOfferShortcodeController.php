d3890016fa4b9ff0f106314d0422dbdc318b4e3e
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2016-08-09 15:44:17 +0300

    Модель шорткода спецпредложения

--- app/includes/controllers/site/shortcodes/TPSpecialOfferShortcodeController.php
+++ app/includes/controllers/site/shortcodes/TPSpecialOfferShortcodeController.php
@@ -13,9 +13,23 @@ class TPSpecialOfferShortcodeController extends \app\includes\controllers\site\T
 {
     public $model;
     public $view;
-
+    public function __construct(){
+        parent::__construct();
+        $this->model = new \app\includes\models\site\shortcodes\TPSpecialOfferShortcodeModel();
+        $this->view = new \app\includes\views\site\shortcodes\TPShortcodeView();
+    }
     public function initShortcode()
     {
         // TODO: Implement initShortcode() method.
+        add_shortcode( 'tp_special_offer_shortcodes', array(&$this, 'actionTable'));
     }
+
+    public function actionTable($args = array())
+    {
+        /*$data = $this->model->getDataTable($args);
+        if(!$data) return false;
+        return $this->view->renderTable($data);*/
+        var_dump("tp_special_offer_shortcodes");
+    }
+
 }
---
+++ app/includes/models/site/shortcodes/TPSpecialOfferShortcodeModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeman
+ * Date: 09.08.16
+ * Time: 15:19
+ */
+
+namespace app\includes\models\site\shortcodes;
+
+
+class TPSpecialOfferShortcodeModel
+{
+
+}
