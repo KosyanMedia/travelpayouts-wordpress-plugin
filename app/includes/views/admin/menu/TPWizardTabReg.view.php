2dc783aa12ed8573c17121db8070ae1aa07667bc
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2016-08-26 11:08:10 +0300

    Новый формат файлов перевода сделал wizard tab reg

--- app/includes/views/admin/menu/TPWizardTabReg.view.php
+++ app/includes/views/admin/menu/TPWizardTabReg.view.php
@@ -1,21 +1,26 @@
 <p class="TP-deteiledIncome TP-deteiledIncomeR">
-    <?php _e('New to Travelpayouts? Please, ', TPOPlUGIN_TEXTDOMAIN);?>
+    <?php _ex('tp_admin_page_wizard_tab_menu_registration_paragraph_1',
+        'admin page wizard tab menu registration paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
     <a href="http://www.travelpayouts.com/?marker=85103&locale=<?php echo $this->local; ?>&utm_source=wpplugin&utm_medium=welcome_page&utm_campaign=<?php echo $this->local; ?>" target="_blank">
-        <?php _e('Register', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('tp_admin_page_wizard_tab_menu_registration_paragraph_1_link_registration',
+            'admin page wizard tab menu registration paragraph_1 link registration', TPOPlUGIN_TEXTDOMAIN); ?>
     </a>
 </p>
 <p class="TP-deteiledIncome">
-    <?php _e('Registration is totally free and takes less than a minute.', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('tp_admin_page_wizard_tab_menu_registration_paragraph_2',
+        'admin page wizard tab menu registration paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
 <p class="TP-deteiledIncome">
-    <?php _e('Return to plugin settings to fill out your Profile.', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('tp_admin_page_wizard_tab_menu_registration_paragraph_3',
+        'admin page wizard tab menu registration paragraph_3', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
 
 <div class="TP-navsPan">
     <!--Кнопка может быть не активной: добавляйте класс disable для достижение такого состояние-->
     <!--<button class="TP-BtnTab">сохранить изменения</button>-->
     <a href="http://www.travelpayouts.com/?marker=85103&locale=<?php echo $this->local; ?>&utm_source=wpplugin&utm_medium=welcome_page&utm_campaign=<?php echo $this->local; ?>&utm_content=button" class="TP-BtnTab TP-BtnTabR" target="_blank">
-        <?php _e('1. Sign up ', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('tp_admin_page_wizard_tab_menu_registration_link_sing_up',
+            'admin page wizard tab menu registration link sing up', TPOPlUGIN_TEXTDOMAIN); ?>
     </a>
 </div>
 
@@ -23,7 +28,8 @@
     <!--Кнопка может быть не активной: добавляйте класс disable для достижение такого состояние-->
     <!--<button class="TP-BtnTab">сохранить изменения</button>-->
     <a class="TP-BtnTab TP-BtnTabR TP-BtnTabWizard " href="#">
-        <?php _e('2. Enter settings ', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('tp_admin_page_wizard_tab_menu_registration_link_enter_settings',
+            'admin page wizard tab menu registration link enter settings', TPOPlUGIN_TEXTDOMAIN); ?>
     </a>
 </div>
 
