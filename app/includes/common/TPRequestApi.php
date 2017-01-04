32c1b27ecce5883758225659b0400658252b353c
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-04 14:16:02 +0200

    TPRequestApiTable класс который отвечает за запросы таблиц

--- app/includes/common/TPRequestApi.php
+++ app/includes/common/TPRequestApi.php
@@ -12,4 +12,14 @@ namespace app\includes\common;
 class TPRequestApi
 {
 
+    private static $instance = null;
+    private function __construct() {
+
+    }
+    public static function getInstance(){
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 }
