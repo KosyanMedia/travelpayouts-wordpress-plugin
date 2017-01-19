8e68c0bebdcc9f18098effc51c949d495dfff03b
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-19 17:16:07 +0200

    Добавил модель шорткода Таблица "Отели Города по цене ОТ-ДО"

---
+++ app/includes/models/site/shortcodes/hotels/TPHotelsCityPriceFromToShortcodeModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 19.01.17
+ * Time: 5:14 PM
+ */
+
+namespace app\includes\models\site\shortcodes\hotels;
+
+use \app\includes\models\site\TPHotelShortcodeModel;
+use \app\includes\common\TPCurrencyUtils;
+
+class TPHotelsCityPriceFromToShortcodeModel extends TPHotelShortcodeModel
+{
+
+    public function get_data($args = array())
+    {
+        // TODO: Implement get_data() method.
+    }
+}
