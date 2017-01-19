f8cd128105038d36e7863d420568a469f018434a
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-19 17:02:20 +0200

    Добавил модель шорткода Таблица "Стоимость проживания В ГОРОДЕ на неделю"

---
+++ app/includes/models/site/shortcodes/hotels/TPCostLivingCityWeekShortcodeModel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 19.01.17
+ * Time: 4:59 PM
+ */
+
+namespace app\includes\models\site\shortcodes\hotels;
+
+use \app\includes\models\site\TPHotelShortcodeModel;
+use \app\includes\common\TPCurrencyUtils;
+
+class TPCostLivingCityWeekShortcodeModel extends TPHotelShortcodeModel
+{
+
+    public function get_data($args = array())
+    {
+        // TODO: Implement get_data() method.
+    }
+}
