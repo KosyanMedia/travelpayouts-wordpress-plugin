53ef5a317a3ca2caadf34475ecf98891ab7c4fab
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-26 15:35:44 +0200

    TPHotelsController

---
+++ app/includes/controllers/admin/menu/TPHotelsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 26.01.17
+ * Time: 3:35 PM
+ */
+
+namespace app\includes\controllers\admin\menu;
+
+
+class TPHotelsController
+{
+
+}
