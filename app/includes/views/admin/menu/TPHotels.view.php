15a08ac97a3504ff2408309fdfe916646fc1c144
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-30 13:54:36 +0200

    TPHotels.view добавил табы

--- app/includes/views/admin/menu/TPHotels.view.php
+++ app/includes/views/admin/menu/TPHotels.view.php
@@ -4,6 +4,33 @@
             '(Hotels)', TPOPlUGIN_TEXTDOMAIN); ?>
     </p>
     <div id="tabs-hotels">
+        <nav class="TPNavigation">
+            <ul class="TPMainMenu">
+                <li>
+                    <a href="#tabs-hotels_config" class="TPMainMenuA">
+                        <i class="icoItemNav ico-table"></i>
+                        <span>
+                            <?php _ex('tp_admin_page_flights_tab_menu_tickets_config',
+                                '(Tables Content)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        </span>
+                    </a>
+                </li>
 
+                <li>
+                    <a href="#tabs-hotels_themes" class="TPMainMenuA">
+                        <i class="icoItemNav ico-glass"></i>
+                        <span>
+                            <?php _ex('tp_admin_page_flights_tab_menu_tickets_themes',
+                                '(Themes)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        </span>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <div id="tabs-hotels_config"></div>
+        <div id="tabs-hotels_themes">
+
+        </div>
     </div>
 </div>
