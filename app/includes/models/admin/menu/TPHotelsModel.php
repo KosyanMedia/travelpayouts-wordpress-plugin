2624c34a28c96e9ff6e927868db6e2fe45000371
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-01-30 10:39:41 +0200

    Класс TPHotelsModel отвечающий за настройки страницы отели в меню плагина

---
+++ app/includes/models/admin/menu/TPHotelsModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 30.01.17
+ * Time: 10:38 AM
+ */
+
+namespace app\includes\models\admin\menu;
+
+
+class TPHotelsModel
+{
+
+}
