9968beb2f0801edb8043085d784dfd4b7177b4ee
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-02-07 14:09:39 +0200

    Класс для кнопки шорткода отеля. Добавил кнопку

--- app/includes/controllers/admin/media_buttons/TPHotelsButtonsController.php
+++ app/includes/controllers/admin/media_buttons/TPHotelsButtonsController.php
@@ -15,6 +15,30 @@ class TPHotelsButtonsController extends TPAdminMediaButtonsController
     public function action($args = array())
     {
         // TODO: Implement action() method.
+        $text = $this->getTextBtn(
+            _x( 'tp_admin_media_button_insert_hotels_title',
+                'admin media button insert hotels title', TPOPlUGIN_TEXTDOMAIN  ),
+            _x( 'tp_admin_media_button_hotels_short_title',
+                'admin media button hotels title', TPOPlUGIN_TEXTDOMAIN  )
+        );
+
+        $args = wp_parse_args( $args, array(
+            'target'    => 'content',
+            'text'      => $text,
+            'class'     => 'button',
+            'icon'      =>  TPOPlUGIN_URL.'app/public/images/tp_button_shortcode_hotel.png',
+            'echo'      => true,
+            'shortcode' => false
+        ) );
+        // Prepare icon
+        if ( $args['icon'] ) $args['icon'] = '<img src="' . $args['icon'] . '" /> ';
+        $button = '<a href="#" id="constructorShortcodesButton" class="su-generator-button '.$args['class'].'">'.
+            $args['icon'] . $args['text'].'</a>';
+        add_action( 'wp_footer',    array( &$this, 'render' ) );
+        add_action( 'admin_footer', array( &$this, 'render' ) );
+        wp_enqueue_media();
+        if ( $args['echo'] ) echo $button;
+        return $button;
     }
 
     public function render()
