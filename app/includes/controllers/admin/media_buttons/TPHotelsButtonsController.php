6a2eef0f21b01d153c84b2caa0583c6605e2d1a1
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-02-07 13:53:26 +0200

    Класс для кнопки шорткода отеля

---
+++ app/includes/controllers/admin/media_buttons/TPHotelsButtonsController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 07.02.17
+ * Time: 13:52
+ */
+
+namespace app\includes\controllers\admin\media_buttons;
+
+
+class TPHotelsButtonsController
+{
+
+}
