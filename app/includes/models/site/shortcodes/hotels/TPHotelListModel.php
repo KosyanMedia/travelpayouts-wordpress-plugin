602900c7a50f02fe9dd6f6189734207558d6ab8e
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-07 13:51:06 +0200

    TPHotelListModel модель для данных от запросса hotelList

--- app/includes/models/site/shortcodes/hotels/TPHotelListModel.php
+++ app/includes/models/site/shortcodes/hotels/TPHotelListModel.php
@@ -9,7 +9,46 @@
 namespace app\includes\models\site\shortcodes\hotels;
 
 
-class TPHotelListModel
+class TPHotelListModel extends \core\models\TPOWPTableModel implements \core\models\TPOWPTableInterfaceModel
 {
 
+    public static function createTable()
+    {
+        // TODO: Implement createTable() method.
+    }
+
+    public static function deleteTable()
+    {
+        // TODO: Implement deleteTable() method.
+    }
+
+    public function insert($data)
+    {
+        // TODO: Implement insert() method.
+    }
+
+    public function update($data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function deleteAll()
+    {
+        // TODO: Implement deleteAll() method.
+    }
+
+    public function deleteId($id)
+    {
+        // TODO: Implement deleteId() method.
+    }
+
+    public function query()
+    {
+        // TODO: Implement query() method.
+    }
+
+    public function get_data()
+    {
+        // TODO: Implement get_data() method.
+    }
 }
