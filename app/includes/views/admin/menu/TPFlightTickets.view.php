126e599d2f65ab00eb5c0acb5ef455ad316c91da
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-22 14:07:31 +0300

    Нумерация тем

--- app/includes/views/admin/menu/TPFlightTickets.view.php
+++ app/includes/views/admin/menu/TPFlightTickets.view.php
@@ -70,6 +70,7 @@
 
                 <div class="TPThemes">
                     <form action="options.php" class="formSettings TPFormNotReload" method="POST">
+                        <?php $numberTheme = 1; ?>
                         <?php foreach($data['themes'] as $theme): ?>
                             <?php
                             $TPThemeActive = ($theme['name'] ==  \app\includes\TPPlugin::$options['themes_table']['name'])?'TPThemeActive':'';
@@ -79,7 +80,10 @@
                                 <div class="TPThemeScreenshot">
                                     <img src="<?php echo TPOPlUGIN_URL.'app/public/themes/flight/screens-and-names/'.$theme['screenshot']?>" alt="">
                                 </div>
-                                <h3 class="TPThemeName"><?php echo $theme['title']; ?></h3>
+                                <h3 class="TPThemeName">
+		                            <?php echo $numberTheme.'. '; ?>
+		                            <?php echo $theme['title']; ?>
+                                </h3>
                                 <div class="TPThemeActions">
                                     <input type="submit" name="submit"
                                            class="button button-secondary activate TPThemeBtnActivate "
@@ -93,6 +97,7 @@
                                     </a>-->
                                 </div>
                             </div>
+	                        <?php $numberTheme++; ?>
                         <?php endforeach; ?>
 
                             <?php settings_fields('TPFlightTickets'); ?>
