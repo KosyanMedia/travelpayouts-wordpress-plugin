c9545e9028dfc94688fcce8838abbc81ef390914
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-23 18:02:14 +0300

    ЖД кнопка для модалки

---
+++ app/includes/controllers/admin/media_buttons/TPRailwayButtonsController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 23.05.17
+ * Time: 18:01
+ */
+
+namespace app\includes\controllers\admin\media_buttons;
+
+
+class TPRailwayButtonsController extends TPAdminMediaButtonsController{
+
+	public function action( $args = array() ) {
+		// TODO: Implement action() method.
+	}
+
+	public function render() {
+		// TODO: Implement render() method.
+	}
+}
