925b1965710620f4cb3c7d32b0353f3b16f1fcea
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-23 14:30:20 +0300

    класс шорткода [tp_tutu]

--- app/includes/controllers/site/shortcodes/railway/TPTutuShortcodeController.php
+++ app/includes/controllers/site/shortcodes/railway/TPTutuShortcodeController.php
@@ -13,7 +13,25 @@ use app\includes\controllers\site\TPShortcodesController;
 
 class TPTutuShortcodeController extends TPShortcodesController {
 
+	public $model;
+	public $view;
+	public function __construct(){
+		parent::__construct();
+		//$this->model = new TPCostLivingCityDaysShortcodeModel();
+		//$this->view = new \app\includes\views\site\shortcodes\TPShortcodeView();
+	}
 	public function initShortcode() {
 		// TODO: Implement initShortcode() method.
+		//Таблица "ЖД"
+		add_shortcode( 'tp_tutu', array(&$this, 'actionTable'));
 	}
+
+	public function actionTable($args = array())
+	{
+
+		//$data = $this->model->getDataTable($args);
+		$data = 'tu tu';
+		return var_dump("<pre>", $data, "</pre>");
+	}
+
 }
