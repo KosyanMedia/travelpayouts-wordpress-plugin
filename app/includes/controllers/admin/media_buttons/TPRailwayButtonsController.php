00ad078617dd977726e1fb3be9a61e48428bd4e0
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-23 18:31:10 +0300

    ЖД кнопка для модалки картинка

--- app/includes/controllers/admin/media_buttons/TPRailwayButtonsController.php
+++ app/includes/controllers/admin/media_buttons/TPRailwayButtonsController.php
@@ -13,6 +13,30 @@ class TPRailwayButtonsController extends TPAdminMediaButtonsController{
 
 	public function action( $args = array() ) {
 		// TODO: Implement action() method.
+		$text = $this->getTextBtn(
+			_x( 'Railway Schedule',
+				'admin media button insert railway title', TPOPlUGIN_TEXTDOMAIN  ),
+			_x( 'Railway Schedule',
+				'admin media button railway title', TPOPlUGIN_TEXTDOMAIN  )
+		);
+
+		$args = wp_parse_args( $args, array(
+			'target'    => 'content',
+			'text'      => $text,
+			'class'     => 'button',
+			'icon'      =>  TPOPlUGIN_URL.'app/public/images/tp_button_shortcode_train.png',
+			'echo'      => true,
+			'shortcode' => false
+		) );
+		// Prepare icon
+		if ( $args['icon'] ) $args['icon'] = '<img src="' . $args['icon'] . '" /> ';
+		$button = '<a href="#" id="constructorRailwayShortcodesButton" class="su-generator-button '
+		          .$args['class'].'">'. $args['icon'] . $args['text'].'</a>';
+		add_action( 'wp_footer',    array( &$this, 'render' ) );
+		add_action( 'admin_footer', array( &$this, 'render' ) );
+		wp_enqueue_media();
+		if ( $args['echo'] ) echo $button;
+		return $button;
 	}
 
 	public function render() {
