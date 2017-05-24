a4dc4db31302ad5a59627a2790ff57b4c708278c
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-24 15:19:07 +0300

    ЖД страница настроек view табы

--- app/includes/views/admin/menu/TPRailway.view.php
+++ app/includes/views/admin/menu/TPRailway.view.php
@@ -3,4 +3,36 @@
 		<?php _ex('Railway tables',
 			'admin page menu railway', TPOPlUGIN_TEXTDOMAIN); ?>
 	</p>
+    <div id="tabs-railway">
+        <nav class="TPNavigation">
+            <ul class="TPMainMenu">
+                <li>
+                    <a href="#tabs-railway_config" class="TPMainMenuA">
+                        <i class="icoItemNav ico-table"></i>
+                        <span>
+                            <?php _ex('Tables Content',
+	                            'admin page railway tab menu railway config',
+                                TPOPlUGIN_TEXTDOMAIN); ?>
+                        </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#tabs-railway_themes" class="TPMainMenuA">
+                        <i class="icoItemNav ico-glass"></i>
+                        <span>
+                            <?php _ex('Themes',
+	                            'admin page railway tab menu railway themes',
+                                TPOPlUGIN_TEXTDOMAIN); ?>
+                        </span>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <div id="tabs-railway_config">
+        </div>
+        <div id="tabs-railway_themes"></div>
+    </div>
+
 </div>
