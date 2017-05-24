4f8b203fee2a89120ee53fcf195e55564136e635
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-24 14:05:19 +0300

    ЖД класс страницы настроек добавление меню

--- app/includes/controllers/admin/menu/TPRailwayController.php
+++ app/includes/controllers/admin/menu/TPRailwayController.php
@@ -12,8 +12,20 @@ use \core\controllers\TPOAdminMenuController;
 
 class TPRailwayController extends TPOAdminMenuController{
 
+	public $model;
+	public function __construct() {
+		parent::__construct();
+	}
+
 	public function action() {
 		// TODO: Implement action() method.
+		$plugin_page = add_submenu_page( TPOPlUGIN_TEXTDOMAIN,
+			_x('Railway tables',  'admin menu page title railway', TPOPlUGIN_TEXTDOMAIN ),
+			_x('Railway tables',  'admin menu page title railway', TPOPlUGIN_TEXTDOMAIN ),
+			'manage_options',
+			'tp_control_railway',
+			array(&$this, 'render'));
+		add_action( 'admin_footer-'.$plugin_page, array(&$this, 'TPLinkHelp') );
 	}
 
 	public function render() {
