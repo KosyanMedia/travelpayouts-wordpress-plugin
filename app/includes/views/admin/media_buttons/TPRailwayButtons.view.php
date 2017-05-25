024a61ef21d6a64e1cf29f189f402b1e996bb29d
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-25 15:23:13 +0300

    ЖД модалка field railway title

--- app/includes/views/admin/media_buttons/TPRailwayButtons.view.php
+++ app/includes/views/admin/media_buttons/TPRailwayButtons.view.php
@@ -1,3 +1,13 @@
 <div id="constructorRailwayShortcodesModal"
      title="<?php _ex('tp_admin_page_settings_сonstructor_tables_title', '(Constructor tables)', TPOPlUGIN_TEXTDOMAIN); ?>" style="display: none;">
+    <table>
+        <tr id="tr_railway_title">
+            <td>
+                <input type="text" name="tp_railway_title" id="tp_railway_title" value=""
+                       class="regular-text code"
+                       placeholder="<?php _ex('Alternate title',
+				           'modal railway сonstructor title label', TPOPlUGIN_TEXTDOMAIN); ?>">
+            </td>
+        </tr>
+    </table>
 </div>
