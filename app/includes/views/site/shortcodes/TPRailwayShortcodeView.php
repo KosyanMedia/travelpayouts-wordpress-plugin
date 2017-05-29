488152518ad80a816f914ec27f53bc0351ae1606
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-29 22:58:55 +0300

    shortcode [tutu] TPRailwayShortcodeView

---
+++ app/includes/views/site/shortcodes/TPRailwayShortcodeView.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: solomashenko
+ * Date: 29.05.17
+ * Time: 22:58
+ */
+
+namespace app\includes\views\site\shortcodes;
+
+
+class TPRailwayShortcodeView {
+
+}
