24b90fa1bbc7b0630ca931e7df58d7b798ef2fda
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-29 22:51:03 +0300

    shortcode [tutu] class model abstract TPRailwayShortcodeModel extends TPShortcodesCaheModel $TPRequestApi init

--- app/includes/models/site/TPRailwayShortcodeModel.php
+++ app/includes/models/site/TPRailwayShortcodeModel.php
@@ -9,6 +9,15 @@
 namespace app\includes\models\site;
 
 
-abstract class TPRailwayShortcodeModel extends TPShortcodesChacheModel{
+use app\includes\common\TPRequestApiRailway;
 
+abstract class TPRailwayShortcodeModel extends TPShortcodesChacheModel{
+	protected static $TPRequestApi;
+	public function __construct()
+	{
+		parent::__construct();
+		//error_log('TPTableShortcodeModel');
+		self::$TPRequestApi = TPRequestApiRailway::getInstance();
+		//error_log(print_r(get_class_methods(self::$TPRequestApi), true));
+	}
 }
