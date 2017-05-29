674d8bd9a3b46bf8df1d98d5cebb7f1529d75425
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-30 00:49:39 +0300

    shortcode [tp_tutu] model method get_data copy hotel

--- app/includes/models/site/shortcodes/railway/TPTutuShortcodeModel.php
+++ app/includes/models/site/shortcodes/railway/TPTutuShortcodeModel.php
@@ -17,6 +17,64 @@ class TPTutuShortcodeModel extends TPRailwayShortcodeModel {
 
 	public function get_data( $args = array() ) {
 		// TODO: Implement get_data() method.
+		$defaults = array(
+			'id' => false,
+			'check_in' => false,
+			'check_out' => false,
+			'currency' => TPCurrencyUtils::getDefaultCurrency(),
+			'language' => TPLang::getLang(),
+			'limit' => 5,
+			'type' => 'popularity',
+			'return_url' => false
+		);
+		extract( wp_parse_args( $args, $defaults ), EXTR_SKIP );
+		$attr = array(
+			'id' => $id,
+			'check_in' => false,
+			'check_out' => false,
+			'currency' => $currency,
+			'language' => $language,
+			'limit' => $limit,
+			'type' => $type,
+			'return_url' => $return_url
+		);
+
+		$cacheKey = "hotel_1_selections_discount_{$currency}_{$language}_{$limit}_{$type}_{$id}";
+		//error_log($cacheKey);
+
+		if($this->cacheSecund() && $return_url == false){
+			if ( false === ($rows = get_transient($this->cacheKey($cacheKey)))) {
+				$return = self::$TPRequestApi->getHotelSelection($attr);
+				$rows = array();
+				$cacheSecund = 0;
+				if( ! $return ) {
+					$rows = array();
+					$cacheSecund = $this->cacheEmptySecund();
+				} else {
+					$rows = $return;
+					$rows = array_shift($rows);
+					$cacheSecund = $this->cacheSecund('hotel');
+				}
+				set_transient( $this->cacheKey($cacheKey) , $rows, $cacheSecund);
+			}
+
+		} else {
+			$rows = self::$TPRequestApi->getHotelSelection($attr);
+			if (!$rows){
+				return false;
+			}
+			if ($return_url == false){
+				$rows = array();
+				$rows = array_shift($rows);
+			}
+
+		}
+
+		//tpErrorLog(print_r($rows, true));
+		//error_log('Discount');
+		//error_log(print_r($rows, true));
+
+		return $rows;
 	}
 
 	public function getDataTable($args = array()){
@@ -39,30 +97,18 @@ class TPTutuShortcodeModel extends TPRailwayShortcodeModel {
 			'return_url' => false,
 			'language' => TPLang::getLang()
 		);
-
-
-
 		extract( wp_parse_args( $args, $defaults ), EXTR_SKIP );
-
 		if ($return_url == 1){
 			$return_url = true;
 		}
-
-
 		$return = $this->get_data(array(
 			'origin' => $origin,
 			'destination' => $destination,
-			'title' => $title,
-			'paginate' => $paginate,
-			'off_title' => $off_title,
-			'subid' => $subid,
 			'currency' => $currency,
 			'return_url' => $return_url,
 			'language' => $language,
 			'shortcode' => 1,
 		));
-
-
 		return array(
 			'rows' => $return,
 			'origin' => $origin,
