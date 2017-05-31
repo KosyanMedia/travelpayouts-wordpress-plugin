c1d24486483fc6f48bf48f77e83d4c2f77738f7b
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-06-01 02:36:52 +0300

    TPRailwayShortcodeView View method renderTable renderTitle

--- app/includes/views/site/shortcodes/TPRailwayShortcodeView.php
+++ app/includes/views/site/shortcodes/TPRailwayShortcodeView.php
@@ -41,23 +41,53 @@ class TPRailwayShortcodeView {
 		$html .= '<div class="TPTrainTable TP-Plugin-Tables_wrapper clearfix TP-HotelsTableWrapper">'
 		         .$this->renderTitleTable($off_title, $title, $shortcode, $city, $city_label,
 				$type_selections_label, $dates_label)
-		         .'<table class="TPTableShortcode TP-Plugin-Tables_box  TP-rwd-table TP-rwd-table-avio"
+		         .'<!--<table class="TPTableShortcode TP-Plugin-Tables_box  TP-rwd-table TP-rwd-table-avio"
                         data-paginate="'.$paginate.'"
                         data-paginate_limit="'
 		                .TPPlugin::$options['shortcodes_railway'][$shortcode]['paginate']
-		         .'" data-sort_column="'.$this->getSortColumn($shortcode).'">'
-		         .$this->renderHeadTable($shortcode)
-		         .$this->renderBodyTable()
-		         .'</table>
+		         .'" data-sort_column="$this->getSortColumn($shortcode)">'
+		         //$this->renderHeadTable($shortcode)
+		         //$this->renderBodyTable()
+		         .'</table>-->
                 </div>';
 
 		return $html;
 
 
-		return var_dump("<pre>", $args, "</pre>");
+		//return var_dump("<pre>", $args, "</pre>");
 	}
 
 	public function renderViewIfEmptyTable(){
+		return '';
+	}
+
+	/**
+	 * @param $off_title
+	 * @param $title
+	 * @param $shortcode
+	 * @param $city
+	 * @return string
+	 */
+	public function renderTitleTable($off_title, $title, $shortcode, $origin, $destination){
+		if($off_title !== 'true'){
+			if(empty($title)) {
+				if(isset(TPPlugin::$options['shortcodes_hotels'][$shortcode]['title'][TPLang::getLang()])){
+					$title = TPPlugin::$options['shortcodes_hotels'][$shortcode]['title'][TPLang::getLang()];
+				}else{
+					$title = \app\includes\TPPlugin::$options['shortcodes_hotels'][$shortcode]['title'][TPLang::getDefaultLang()];
+				}
+			}
+			if(!empty($title)){
+
+				if(strpos($title, '{location}') !== false){
+					$title = str_replace('{location}', '<span data-city-location="'.$cityLabel.'">'.$cityLabel.'</span>' , $title);
+				}
+
+
+
+			}
+			return '<'.\app\includes\TPPlugin::$options['shortcodes'][$shortcode]['tag'].' class="TP-TitleTables">'.$title.'</'.\app\includes\TPPlugin::$options['shortcodes'][$shortcode]['tag'].'>';
+		}
 		return '';
 	}
 }
