16132c363e8133def6d0cc1ca036793033dd0900
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-05-31 23:31:59 +0300

    Settings options Railway tab config add section

--- app/includes/models/admin/menu/TPRailwayModel.php
+++ app/includes/models/admin/menu/TPRailwayModel.php
@@ -23,5 +23,9 @@ class TPRailwayModel extends TPBaseShortcodeOptionModel {
 		add_settings_section( 'tp_settings_railway_themes_table_id', '', '', 'tp_settings_railway_themes_table' );
 		add_settings_field('tp_railway_themes_table_td', '', array(&$field ,'TPFieldThemesTable'),
 			'tp_settings_railway_themes_table', 'tp_settings_railway_themes_table_id' );
+
+		add_settings_section( 'tp_settings_railway_shortcode_1_id', '', '', 'tp_settings_railway_shortcode_1' );
+		add_settings_field('tp_railway_shortcode_1_td', '', array(&$field ,'TPFieldShortcode_1'),
+			'tp_settings_railway_shortcode_1', 'tp_settings_railway_shortcode_1_id' );
 	}
 }
--- app/includes/views/admin/menu/TPRailwayTabConfig.view.php
+++ app/includes/views/admin/menu/TPRailwayTabConfig.view.php
@@ -1,5 +1,6 @@
 <form action="options.php" class="formSettings TPFormNotReload" method="POST" id="TPHotelsConfig">
 	<?php settings_fields('TPRailway'); ?>
 	<div class="TPmainContent TP-BalanceContent">
+		<?php do_settings_fields('tp_settings_railway_shortcode_1', 'tp_settings_railway_shortcode_1_id'); ?>
 	</div>
 </form>
