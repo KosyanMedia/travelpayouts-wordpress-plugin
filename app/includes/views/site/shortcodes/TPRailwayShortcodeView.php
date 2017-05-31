a779685c97a0c22c7d9a7994c598fac22d6663ad
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-06-01 02:03:20 +0300

    TPRailwayShortcodeView View method renderTable defaults

--- app/includes/views/site/shortcodes/TPRailwayShortcodeView.php
+++ app/includes/views/site/shortcodes/TPRailwayShortcodeView.php
@@ -9,8 +9,34 @@
 namespace app\includes\views\site\shortcodes;
 
 
+use app\includes\common\TPLang;
+use app\includes\TPPlugin;
+
 class TPRailwayShortcodeView {
+
+	public function __construct()
+	{
+
+	}
+
 	public function renderTable($args = array()) {
+		$defaults = array(
+			'rows' => array(),
+			'origin' => '',
+			'destination' => '',
+			'title' => '',
+			'paginate' => 'false',
+			'off_title' => '',
+			'subid' => '',
+			'currency' => TPPlugin::$options['local']['currency'],
+			'return_url' => false,
+			'language' => TPLang::getLang(),
+			'shortcode' => null
+		);
+		extract( wp_parse_args( $args, $defaults ), EXTR_SKIP );
+		//error_log('currency = '.$currency);
+		$html = '';
+
 		return var_dump("<pre>", $args, "</pre>");
 	}
 }
