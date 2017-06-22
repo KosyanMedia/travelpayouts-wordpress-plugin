e0bea83893bde8a65017014e619356e5eba31869
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-06-22 17:49:17 +0300

    Railway правки merge js

--- app/public/js/site/TPPlugin.js
+++ app/public/js/site/TPPlugin.js
@@ -407,15 +407,16 @@ jQuery(function($) {
 
 
 
-<<<<<<< HEAD
+
 
     $(document).ready(function () {
         var width_departure = $(".TPTrainTable .TP-Plugin-Tables_box_thead tr td:nth-child(2)").width();
         if(width_departure<="128"){
             $(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отпра-вление");
-        }else{
+        }else {
             $(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отправление");
-=======
+        }
+    });
     
     
     $(document).ready(function () {
@@ -424,21 +425,21 @@ jQuery(function($) {
            	$(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отпра-вление");
         }else{
            	$(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отправление");
->>>>>>> f9452354347e7692243e650e1e602ef2dd15692a
+
         }
     });
     $(document).resize(function () {
         var width_departure = $(".TPTrainTable .TP-Plugin-Tables_box_thead tr td:nth-child(2)").width();
         if(width_departure<="128"){
-<<<<<<< HEAD
+
             $(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отпра-вление");
         }else{
             $(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отправление");
-=======
+
            	$(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отпра-вление");
         }else{
            	$(".TPTrainTable .TPdepartureTd.TPTableHead.sorting span").text("Отправление");
->>>>>>> f9452354347e7692243e650e1e602ef2dd15692a
+
         }
     });
 });
