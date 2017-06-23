f874ce2c34f4631fa39c779f716ad73b675692dd
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-06-23 15:11:29 +0300

    Railway TPRailwayTabHelp content

--- app/includes/views/admin/menu/TPRailwayTabHelp.view.php
+++ app/includes/views/admin/menu/TPRailwayTabHelp.view.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: solomashenko
- * Date: 23.06.17
- * Time: 15:09
- */
+<div class="TPmainContent TP-BalanceContent TPRailwayContent">
+	<div class="TP-StyleItem">
+		<p>
+			<?php _ex('You need to activate ',
+				'admin page railway tab railway help',
+				TPOPlUGIN_TEXTDOMAIN); ?>
+			<a href="https://www.travelpayouts.com/campaigns/45" target="_blank">
+				<?php _ex('Tutu.ru campaign ',
+					'admin page railway tab railway help',
+					TPOPlUGIN_TEXTDOMAIN); ?>
+			</a>
+			<?php _ex('at Travelpayouts.com beforehand. Links won\'t work without campaign activation. ',
+				'admin page railway tab railway help',
+				TPOPlUGIN_TEXTDOMAIN); ?>
+		</p>
+		<p>
+			<a href="https://www.travelpayouts.com/campaigns/45" target="_blank">
+				<?php _ex('Activate Tutu.ru campaign ',
+					'admin page railway tab railway help',
+					TPOPlUGIN_TEXTDOMAIN); ?>
+			</a>
+		</p>
+
+
+	</div>
+	<div class="TP-navsPan">
+		<a href="#" class="TP-BtnTab tp-help-railway-active">
+			<i></i>
+			<?php _ex('Tutu.ru campaign is activated. Let\'s go',
+				'admin page railway tab railway help',
+				TPOPlUGIN_TEXTDOMAIN); ?>
+		</a>
+	</div>
+</div>
