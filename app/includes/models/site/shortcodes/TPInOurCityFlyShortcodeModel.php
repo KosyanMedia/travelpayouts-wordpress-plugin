3a1e78e3687f7da3262c9d3f487a20f3f7293fa6
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-08-22 18:45:41 +0300

    Flight Table 11 widget default param

--- app/includes/models/site/shortcodes/TPInOurCityFlyShortcodeModel.php
+++ app/includes/models/site/shortcodes/TPInOurCityFlyShortcodeModel.php
@@ -96,7 +96,8 @@ class TPInOurCityFlyShortcodeModel extends TPFlightShortcodeModel{
             'paginate' => true,
             'off_title' => '',
             'subid' => '',
-            'return_url' => false
+            'return_url' => false,
+            'widget' => 0
         );
         extract(wp_parse_args($args, $defaults), EXTR_SKIP);
         if ($return_url == 1){
