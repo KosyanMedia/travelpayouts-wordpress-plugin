64e7d2736355180d167e859cd5f7e7400387e168
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-08-22 18:52:04 +0300

    Flight Table 2 widget default param

--- app/includes/models/site/shortcodes/TPPriceCalendarWeekShortcodeModel.php
+++ app/includes/models/site/shortcodes/TPPriceCalendarWeekShortcodeModel.php
@@ -84,7 +84,8 @@ class TPPriceCalendarWeekShortcodeModel extends TPFlightShortcodeModel{
             'paginate' => true,
             'off_title' => '',
             'subid' => '',
-            'return_url' => false
+            'return_url' => false,
+            'widget' => 0
             );
         extract( wp_parse_args( $args, $defaults ), EXTR_SKIP );
         if ($return_url == 1){
