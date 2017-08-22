0e587dd943c4c785ed2636de0c2658e4258344c2
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-08-22 19:22:18 +0300

    Flight Table Model 13 method get_data call method caheKey send param widget

--- app/includes/models/site/shortcodes/TPFromOurCityFlyShortcodeModel.php
+++ app/includes/models/site/shortcodes/TPFromOurCityFlyShortcodeModel.php
@@ -36,7 +36,7 @@ class TPFromOurCityFlyShortcodeModel extends TPFlightShortcodeModel{
         if($this->cacheSecund() && $return_url == false){
             if(TPOPlUGIN_ERROR_LOG)
                 error_log("{$method} cache");
-            if ( false === ($rows = get_transient($this->cacheKey('13'.$one_way.$currency, $origin)))) {
+            if ( false === ($rows = get_transient($this->cacheKey('13'.$one_way.$currency, $origin, $widget)))) {
                 if(TPOPlUGIN_ERROR_LOG)
                     error_log("{$method} cache false");
                 $return = self::$TPRequestApi->get_latest($attr);
@@ -57,7 +57,7 @@ class TPFromOurCityFlyShortcodeModel extends TPFlightShortcodeModel{
                 if(TPOPlUGIN_ERROR_LOG)
                     error_log("{$method} cache secund = ".$cacheSecund);
 
-                set_transient( $this->cacheKey('13'.$one_way.$currency, $origin) , $rows, $cacheSecund);
+                set_transient( $this->cacheKey('13'.$one_way.$currency, $origin, $widget) , $rows, $cacheSecund);
             }
         }else{
             $return = self::$TPRequestApi->get_latest($attr);
