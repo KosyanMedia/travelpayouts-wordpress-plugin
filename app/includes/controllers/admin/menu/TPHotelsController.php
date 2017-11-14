841ecbeff4518e7f07987dfa5b2272d09821d7a5
Solomashenko Roman <solomashenkor@digicode.net>
2017-11-14 14:34:48 +0200

    New translate menu Hotels  controller

--- app/includes/controllers/admin/menu/TPHotelsController.php
+++ app/includes/controllers/admin/menu/TPHotelsController.php
@@ -24,8 +24,8 @@ class TPHotelsController extends TPOAdminMenuController
     {
         // TODO: Implement action() method.
         $plugin_page = add_submenu_page( TPOPlUGIN_TEXTDOMAIN,
-            _x('tp_admin_menu_page_hotels_title',  'admin menu page title hotels', TPOPlUGIN_TEXTDOMAIN ),
-            _x('tp_admin_menu_page_hotels_title',  'admin menu page title hotels', TPOPlUGIN_TEXTDOMAIN ),
+            _x('Hotels',  'admin menu page title hotels', TPOPlUGIN_TEXTDOMAIN ),
+            _x('Hotels',  'admin menu menu title hotels', TPOPlUGIN_TEXTDOMAIN ),
             'manage_options',
             'tp_control_hotels',
             array(&$this, 'render'));
