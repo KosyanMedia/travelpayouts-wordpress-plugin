386b408b5c4a6f75448c89db7843499ffaecb3ee
Solomashenko Roman <solomashenkor@digicode.net>
2017-11-20 10:39:43 +0200

    TPModalAdminNotice.view.php translate

--- app/includes/views/admin/TPModalAdminNotice.view.php
+++ app/includes/views/admin/TPModalAdminNotice.view.php
@@ -1,7 +1,6 @@
 <div id="adminNoticeModal" class="notice" style="display: none;">
     <!--<p></p>
     <button type="button" class="notice-dismiss"></button>-->
-    <p><?php _ex('tp_admin_page_admin_notice_modal_label',
-            '(Settings saved.)', TPOPlUGIN_TEXTDOMAIN ); ?></p>
+    <p><?php _ex('Settings saved.','tp admin page admin notice modal label', TPOPlUGIN_TEXTDOMAIN ); ?></p>
     <button class="TPnotice-dismiss" id="adminNoticeModalClose"></button>
 </div>
