b22be959c416308345c8c374065d2253afb83da4
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-11-21 07:55:49 +0200

    TPSearchForms.view.php new translate

--- app/includes/views/admin/menu/TPSearchForms.view.php
+++ app/includes/views/admin/menu/TPSearchForms.view.php
@@ -1,14 +1,14 @@
 <div class="TPWrapper TPWrapper-long">
 
     <p class="TPMainTitle TPMainTitleSF">
-        <?php _ex('tp_admin_page_search_forms_paragraph_1',
-            '(Search forms)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Search forms',
+            'tp_admin_page_search_forms_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
     </p>
 
     <div class="TP-TopImportantInfo TP-shortDescription">
         <p>
-            <?php _ex('tp_admin_page_search_forms_paragraph_2',
-                '(Here you can add shortcodes for each search form added to your Travelpayouts account)', TPOPlUGIN_TEXTDOMAIN); ?>
+            <?php _ex('Here you can add shortcodes for each search form added to your Travelpayouts account',
+                'tp_admin_page_search_forms_paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
             <?php
             global $locale;
             $link_help = '';
@@ -17,8 +17,8 @@
                     $link_help = 'https://support.travelpayouts.com/hc/ru/articles/207794617?utm_source=wpplugin&utm_medium=forms&utm_campaign=ru#11';
                     ?>
                     <a href="https://www.travelpayouts.com/tools/forms?utm_source=wpplugin&utm_medium=forms&utm_campaign=ru" target="_blank">
-                        <?php _ex('tp_admin_page_search_forms_paragraph_2_link',
-                            '(https://www.travelpayouts.com/tools/forms)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        <?php _ex('https://www.travelpayouts.com/tools/forms',
+                            'tp_admin_page_search_forms_paragraph_2_link', TPOPlUGIN_TEXTDOMAIN); ?>
                     </a>
                     <?php
                     break;
@@ -26,8 +26,8 @@
                     $link_help = 'https://support.travelpayouts.com/hc/en-us/articles/207794617?utm_source=wpplugin&utm_medium=forms&utm_campaign=en#11';
                     ?>
                     <a href="https://www.travelpayouts.com/tools/forms?utm_source=wpplugin&utm_medium=forms&utm_campaign=en" target="_blank">
-                        <?php _ex('tp_admin_page_search_forms_paragraph_2_link',
-                            '(https://www.travelpayouts.com/tools/forms)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        <?php _ex('https://www.travelpayouts.com/tools/forms',
+                            'tp_admin_page_search_forms_paragraph_2_link', TPOPlUGIN_TEXTDOMAIN); ?>
                     </a>
                     <?php
                     break;
@@ -35,16 +35,16 @@
                     $link_help = 'https://support.travelpayouts.com/hc/en-us/articles/207794617?utm_source=wpplugin&utm_medium=forms&utm_campaign=en#11';
                     ?>
                     <a href="https://www.travelpayouts.com/tools/forms?utm_source=wpplugin&utm_medium=forms&utm_campaign=en" target="_blank">
-                        <?php _ex('tp_admin_page_search_forms_paragraph_2_link',
-                            '(https://www.travelpayouts.com/tools/forms)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        <?php _ex('https://www.travelpayouts.com/tools/forms',
+                            'tp_admin_page_search_forms_paragraph_2_link', TPOPlUGIN_TEXTDOMAIN); ?>
                     </a>
                     <?php
                     break;
             } ?>
         </p>
         <p>
-            <?php _ex('tp_admin_page_search_forms_paragraph_3',
-                '(Check our step-by-step manual )', TPOPlUGIN_TEXTDOMAIN); ?>
+            <?php _ex('Check our step-by-step manual',
+                'tp_admin_page_search_forms_paragraph_3', TPOPlUGIN_TEXTDOMAIN); ?>
             <?php
                 global $locale;
                 $linkHere = '';
@@ -61,35 +61,35 @@
                 }
             ?>
             <a href="<?php echo $linkHere; ?>" target="_blank">
-                <?php _ex('tp_admin_page_search_forms_paragraph_3_link',
-                    '(here )', TPOPlUGIN_TEXTDOMAIN); ?>
+                <?php _ex('here',
+                    'tp_admin_page_search_forms_paragraph_3_link', TPOPlUGIN_TEXTDOMAIN); ?>
             </a>
         </p>
     </div>
 
     <div class="TPmainContent TP-BalanceContent TP-SettingContent">
         <p class="TP-SettingTitle">
-            <?php _ex('tp_admin_page_search_forms_paragraph_4',
-                '(Search Form Shortcodes)', TPOPlUGIN_TEXTDOMAIN); ?>
+            <?php _ex('Search Form Shortcodes',
+                'tp_admin_page_search_forms_paragraph_4', TPOPlUGIN_TEXTDOMAIN); ?>
         </p>
 
         <div class="TP-navsShort">
             <div class="TP-lincksNavShort">
                 <a href="admin.php?page=tp_control_search_shortcodes&action=add_search_shortcode" class="TP-addShortLincks">
                     <i></i>
-                    <?php _ex('tp_admin_page_search_forms_btn_add_shortcode',
-                        '(Add a shortcode)', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Add a shortcode',
+                        'tp_admin_page_search_forms_btn_add_shortcode', TPOPlUGIN_TEXTDOMAIN); ?>
                 </a>
                 <a href="admin.php?page=tp_control_search_shortcodes&action=add_search_shortcode" class="TP-deleteShortLincks deleteChecked"
                    data-type="search_shortcodes">
                     <i></i>
-                    <?php _ex('tp_admin_page_search_forms_btn_remove',
-                        '(Remove)', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Remove',
+                        'tp_admin_page_search_forms_btn_remove', TPOPlUGIN_TEXTDOMAIN); ?>
                 </a>
             </div>
             <a class="TP-AllLincksShort" href="javascript:void(0)">
-                <?php _ex('tp_admin_page_search_forms_btn_all',
-                    '(All)', TPOPlUGIN_TEXTDOMAIN); ?>
+                <?php _ex('All',
+                    'tp_admin_page_search_forms_btn_all', TPOPlUGIN_TEXTDOMAIN); ?>
                 (<span><?php echo count($this->data); ?></span>)
             </a>
         </div>
@@ -102,16 +102,16 @@
                     <label for="chekTableS-all"></label>
                 </td>
                 <td class="TPTableHead">
-                    <?php _ex('tp_admin_page_search_forms_table_td_1',
-                        '(Title )', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Title',
+                        'tp_admin_page_search_forms_table_td_1', TPOPlUGIN_TEXTDOMAIN); ?>
                 </td>
                 <td class="TPTableHead tp-date-column">
-                    <?php _ex('tp_admin_page_search_forms_table_td_2',
-                        '(Add Date )', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Add Date',
+                        'tp_admin_page_search_forms_table_td_2', TPOPlUGIN_TEXTDOMAIN); ?>
                 </td>
                 <td class="tp-notsort-column">
-                    <?php _ex('tp_admin_page_search_forms_table_td_3',
-                        '(Shortcode )', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Shortcode',
+                        'tp_admin_page_search_forms_table_td_3', TPOPlUGIN_TEXTDOMAIN); ?>
                 </td>
                 <td class="tp-notsort-column"></td>
             </tr>
@@ -127,8 +127,8 @@
                         </td>
                         <td>
                             <a href="admin.php?page=tp_control_search_shortcodes&action=edit_search_shortcode&id=<?php echo $record['id'];?>"
-                               class="row-title" title="<?php _ex('tp_admin_page_search_forms_table_edit_link',
-                                '(Edit)', TPOPlUGIN_TEXTDOMAIN); ?>«<?php echo $record['title'];?>»">
+                               class="row-title" title="<?php _ex('Edit',
+                                'tp_admin_page_search_forms_table_edit_link', TPOPlUGIN_TEXTDOMAIN); ?>«<?php echo $record['title'];?>»">
                                 <?php echo $record['title'];?></a>
                         </td>
                         <td>
