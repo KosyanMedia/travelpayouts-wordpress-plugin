8bec8455b976e384bfbb32eb73f19b1483dd41b1
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-11-21 09:36:50 +0200

    TPStatistic.view.php new translate

--- app/includes/views/admin/menu/TPStatistic.view.php
+++ app/includes/views/admin/menu/TPStatistic.view.php
@@ -1,8 +1,8 @@
 <div class="TPWrapper TPWrapper-long">
     <div id="tabs-statistic">
         <p class="TPMainTitle">
-            <?php _ex('tp_admin_page_statistics_paragraph_1',
-                '(Statistics)', TPOPlUGIN_TEXTDOMAIN); ?>
+            <?php _ex('Statistics',
+                'tp_admin_page_statistics_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
         </p>
         <nav class="TPNavigation">
             <ul class="TPMainMenu">
@@ -10,8 +10,8 @@
                     <a href="#tabs-report" class="TPMainMenuA">
                         <i class="icoItemNav ico-report"></i>
                         <span>
-                            <?php _ex('tp_admin_page_statistics_tab_menu_report',
-                                '(Earnings Report)', TPOPlUGIN_TEXTDOMAIN); ?>
+                            <?php _ex('Earnings Report',
+                                'tp_admin_page_statistics_tab_menu_report', TPOPlUGIN_TEXTDOMAIN); ?>
                         </span>
                     </a>
                 </li>
@@ -19,8 +19,8 @@
                     <a href="#tabs-balance" class="TPMainMenuA">
                         <i class="icoItemNav ico-balance"></i>
                         <span>
-                            <?php _ex('tp_admin_page_statistics_tab_menu_balance',
-                                '(Balance and Payouts)', TPOPlUGIN_TEXTDOMAIN); ?>
+                            <?php _ex('Balance and Payouts',
+                                'tp_admin_page_statistics_tab_menu_balance', TPOPlUGIN_TEXTDOMAIN); ?>
                         </span>
                     </a>
                 </li>
@@ -29,20 +29,20 @@
         <div id="tabs-report">
             <div class="TPmainContent TP-BalanceContent">
                 <p class="TP-SettingTitle">
-                    <?php _ex('tp_admin_page_statistics_tab_report_paragraph_1',
-                        '(Earnings Report)', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Earnings Report',
+                        'tp_admin_page_statistics_tab_report_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
                 </p>
                 <p class="infoLittleReport">
-                    <?php _ex('tp_admin_page_statistics_tab_report_paragraph_2',
-                        '(Your Earnings Report for )', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Your Earnings Report for',
+                        'tp_admin_page_statistics_tab_report_paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
                     <span id="infoDateReport"><?php echo date_i18n( 'F Y', time() ); ?></span>
                 </p>
 
                 <div class="TP-ViewStatistics">
                     <label class="TP-ViewStatisticsDate">
                         <span>
-                            <?php _ex('tp_admin_page_statistics_tab_report_select_month',
-                                '(Select month )', TPOPlUGIN_TEXTDOMAIN); ?>
+                            <?php _ex('Select month',
+                                'tp_admin_page_statistics_tab_report_select_month', TPOPlUGIN_TEXTDOMAIN); ?>
                         </span>
                         <?php
                         global $wp_locale;
@@ -62,36 +62,36 @@
                         </select>
                     </label>
                     <button class="TP-BtnTab exportBtn btnBalance btnRep TPGetSalesDate">
-                        <?php _ex('tp_admin_page_statistics_tab_report_btn_show',
-                            '(show)', TPOPlUGIN_TEXTDOMAIN); ?>
+                        <?php _ex('show',
+                            'tp_admin_page_statistics_tab_report_btn_show', TPOPlUGIN_TEXTDOMAIN); ?>
                     </button>
                 </div>
                 <?php echo \app\includes\views\admin\menu\TPStatisticView::tableReport($this->model->detailed_sales); ?>
                 <a download="TPListReport.xls" href="#"  class="TP-BtnTab exportBtn btnBalance"
-                   onclick="return ExcellentExport.excel(this, 'TPListReport', '<?php _ex('tp_admin_page_statistics_tab_report_title_xls',
-                       '(Report on income)', TPOPlUGIN_TEXTDOMAIN); ?>');">
-                    <?php _ex('tp_admin_page_statistics_tab_report_btn_download_xls',
-                        '(DOWNLOAD REPORT (XLS))', TPOPlUGIN_TEXTDOMAIN); ?>
+                   onclick="return ExcellentExport.excel(this, 'TPListReport', '<?php _ex('Report on income',
+                       'tp_admin_page_statistics_tab_report_title_xls', TPOPlUGIN_TEXTDOMAIN); ?>');">
+                    <?php _ex('DOWNLOAD REPORT (XLS)',
+                        'tp_admin_page_statistics_tab_report_btn_download_xls', TPOPlUGIN_TEXTDOMAIN); ?>
                 </a>
             </div>
         </div>
         <div id="tabs-balance">
             <div class="TPmainContent TP-BalanceContent">
                 <p class="TP-SettingTitle">
-                    <?php _ex('tp_admin_page_statistics_tab_balance_paragraph_1',
-                        '(Balance and Payouts)', TPOPlUGIN_TEXTDOMAIN); ?>
+                    <?php _ex('Balance and Payouts',
+                        'tp_admin_page_statistics_tab_balance_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
                 </p>
                 <p class="TP-OurBalance">
-                    <?php _ex('tp_admin_page_statistics_tab_balance_paragraph_2',
-                        '(Balance)', TPOPlUGIN_TEXTDOMAIN); ?>: <span>
+                    <?php _ex('Balance',
+                        'tp_admin_page_statistics_tab_balance_paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>: <span>
                             <?php echo $this->view->balanceLabel(); ?>
                         </span></p>
                 <?php echo $this->view->tableBalance($this->model->payments); ?>
                 <a download="TPListBalance.xls" href="#"  class="TP-BtnTab exportBtn btnBalance"
-                   onclick="return ExcellentExport.excel(this, 'TPListBalance', '<?php _ex('tp_admin_page_statistics_tab_balance_title_xls',
-                       '(Balance and payments)', TPOPlUGIN_TEXTDOMAIN); ?>');">
-                    <?php _ex('tp_admin_page_statistics_tab_balance_btn_download_xls',
-                        '(DOWNLOAD REPORT (XLS))', TPOPlUGIN_TEXTDOMAIN); ?>
+                   onclick="return ExcellentExport.excel(this, 'TPListBalance', '<?php _ex('Balance and payments',
+                       'tp_admin_page_statistics_tab_balance_title_xls', TPOPlUGIN_TEXTDOMAIN); ?>');">
+                    <?php _ex('DOWNLOAD REPORT (XLS)',
+                        'tp_admin_page_statistics_tab_balance_btn_download_xls', TPOPlUGIN_TEXTDOMAIN); ?>
                 </a>
             </div>
         </div>
