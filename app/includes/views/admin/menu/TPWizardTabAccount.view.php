81efedefc5e0da51c38495f4a638586fc80d807b
Solomashenko Roman <solomashenkor@digicode.net>
2017-11-21 13:01:23 +0200

    TPWidgetsTabAcount.view.php new translate

--- app/includes/views/admin/menu/TPWizardTabAccount.view.php
+++ app/includes/views/admin/menu/TPWizardTabAccount.view.php
@@ -2,24 +2,24 @@
     <?php settings_fields('TPWizard'); ?>
     <div class="TPmainContent TPmainContentWizard TP-SettingContent">
         <p class="titleSortable titleSortableR">
-            <?php _ex('tp_admin_page_wizard_tab_menu_account_paragraph_1',
-                'admin page wizard tab menu account paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
+            <?php _ex('Get your API Token and Partner Marker by following',
+                'tp_admin_page_wizard_tab_menu_account_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
             <a href="https://www.travelpayouts.com/developers/api?utm_source=wpplugin&utm_medium=welcome_page&utm_campaign=<?php echo $this->local; ?>" target="_blank">
-                <?php _ex('tp_admin_page_wizard_tab_menu_account_paragraph_1_link_tp',
-                    'admin page wizard tab menu account paragraph_1 link tp', TPOPlUGIN_TEXTDOMAIN); ?>
+                <?php _ex('this link.',
+                    'tp_admin_page_wizard_tab_menu_account_paragraph_1_link_tp', TPOPlUGIN_TEXTDOMAIN); ?>
             </a>
         </p>
         <?php do_settings_fields('tp_settings_wizard', 'tp_settings_wizard_id'); ?>
         <div class="TP-navsPan">
             <?php if(empty(\app\includes\TPPlugin::$options['account']['marker'])){ ?>
                 <span class="TP-msgSend">
-                        <?php _ex('tp_admin_page_wizard_tab_menu_account_paragraph_2',
-                            'admin page wizard tab menu account paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
+                        <?php _ex('By pressing the button "Save" you agree to send the plugin\'s activation data to Travelpayouts.',
+                            'tp_admin_page_wizard_tab_menu_account_paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
                     </span>
             <?php } ?>
             <input type="submit" name="submit" id="TPSaveSettingsWizard" class="TP-BtnTab TP-BtnTabR"
-                   value="<?php _ex('tp_admin_page_wizard_tab_menu_account_btn_save',
-                       'admin page wizard tab menu account btn_save', TPOPlUGIN_TEXTDOMAIN); ?>">
+                   value="<?php _ex('Save changes',
+                       'tp_admin_page_wizard_tab_menu_account_btn_save', TPOPlUGIN_TEXTDOMAIN); ?>">
         </div>
         <?php
             $pathView = TPOPlUGIN_DIR."/app/includes/views/admin/menu/TPWizardTabContent.view.php";
