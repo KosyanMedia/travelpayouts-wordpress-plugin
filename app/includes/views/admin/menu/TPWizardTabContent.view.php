8fa07ef7ef4ec6ea5b4e0830d73e4b82e824d0dc
Solomashenko Roman <solomashenkor@digicode.net>
2017-11-21 13:52:25 +0200

    TPWizardTabContent.view.php new translate

--- app/includes/views/admin/menu/TPWizardTabContent.view.php
+++ app/includes/views/admin/menu/TPWizardTabContent.view.php
@@ -4,88 +4,88 @@
             Travelpayouts
         </a>
     </strong>
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_1',
-        'admin page wizard tab menu content paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex(' – Affiliate Network that monetizes your Travel Traffic. You will earn up to 70% from the commission advertiser gets for each sale. On average, our Affiliate earns 7$ from each sold air ticket and 20$ from each sold hotel booking (with booking amount that equals to approx. 400$).',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_1', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
 <p class="TP-deteiledIncome TP-deteiledIncomeR">
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_2',
-        'admin page wizard tab menu content paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('All user purchases made within 30 days after following links in tables or using placed widgets will be credited to your account.',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_2', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
 <p class="TP-deteiledIncome TP-deteiledIncomeR TP-deteiledIncomeROLTitle">
     <strong>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_3',
-            'admin page wizard tab menu content paragraph_3', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('How it works?',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_3', TPOPlUGIN_TEXTDOMAIN); ?>
     </strong>
 </p>
 <ol class="TP-deteiledIncome TP-deteiledIncomeR TP-deteiledIncomeROL">
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_1',
-            'admin page wizard tab menu content paragraph_3_list_li_1', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('<strong>Install our Affiliate tools.</strong>  You place our widgets and tables right in your posts and help visitors to book favorable flights, hotels and other services.',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_1', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_2',
-            'admin page wizard tab menu content paragraph_3_list_li_2', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('<strong>Visitor pays for the service.</strong>  The client is happy to grab a great deal and gets to have an unforgettable journey, while we pay you a reward.',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_2', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_3',
-            'admin page wizard tab menu content paragraph_3_list_li_3', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('<strong>You get profits.</strong>  Your earnings will be displayed in your Travelpayouts Dashboard and in your plugin. We make scheduled, automated, monthly payouts.',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_3_list_li_3', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
 </ol>
 <p class="TP-deteiledIncome TP-deteiledIncomeRS">
     <strong>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_4',
-            'admin page wizard tab menu content paragraph_4', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('What features does a plugin have?',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_4', TPOPlUGIN_TEXTDOMAIN); ?>
     </strong>
 </p>
 <p class="TP-deteiledIncome TP-deteiledIncomeRS">
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5',
-        'admin page wizard tab menu content paragraph_5', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('Plugin allows to get the following tools added to your Wordpress posts:',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_5', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
 
 <ul class="TP-deteiledIncome TP-deteiledIncomeUL ">
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_1',
-            'admin page wizard tab menu content paragraph_5_list_li_1 (12 customizable tables with flight information;)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('12 customizable tables with flight information;',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_1', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_2',
-            'admin page wizard tab menu content paragraph_5_list_li_2 (Search Forms;)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Search Forms;',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_2', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_3',
-            'admin page wizard tab menu content paragraph_5_list_li_3 (Low cost flights and hotels Map)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Low cost flights and hotels Map',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_3', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_4',
-            'admin page wizard tab menu content paragraph_5_list_li_4 (Low prices Calendar)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Low prices Calendar',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_4', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_5',
-            'admin page wizard tab menu content paragraph_5_list_li_5 (Hotel Widget )', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Hotel Widget',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_5', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
     <li>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_6',
-            'admin page wizard tab menu content paragraph_5_list_li_6 (Popular flight destinations Widget)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('Popular flight destinations Widget',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_5_list_li_6', TPOPlUGIN_TEXTDOMAIN); ?>
     </li>
 </ul>
 
 <p class="TP-deteiledIncome TP-deteiledIncomeRS">
     <strong>
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_6',
-            '(How can I earn more?)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('How can I earn more?',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_6', TPOPlUGIN_TEXTDOMAIN); ?>
     </strong>
 </p>
 <p class="TP-deteiledIncome TP-deteiledIncomeR">
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_7',
-        '(We recommend that you get acquainted to the )', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('We recommend that you get acquainted to the',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_7', TPOPlUGIN_TEXTDOMAIN); ?>
     <a href="https://support.travelpayouts.com/hc/<?php echo $this->local_url; ?>/articles/207794617?utm_source=wpplugin&utm_medium=welcome_page&utm_campaign=<?php echo $this->local; ?>" target="_blank">
-        <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_7_link',
-            '(detailed information)', TPOPlUGIN_TEXTDOMAIN); ?>
+        <?php _ex('detailed information',
+            'tp_admin_page_wizard_tab_menu_content_paragraph_7_link', TPOPlUGIN_TEXTDOMAIN); ?>
     </a>
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_7_1',
-        '(on Travelpayouts WP plugin features. )', TPOPlUGIN_TEXTDOMAIN); ?>
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_7_2',
-        '(Dozens of useful articles in Travelpayouts Knowledge Base, always available at )', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('on Travelpayouts WP plugin features.',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_7_1', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('Dozens of useful articles in Travelpayouts Knowledge Base, always available at',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_7_2', TPOPlUGIN_TEXTDOMAIN); ?>
 
     :
     <a href="https://support.travelpayouts.com/hc/<?php echo $this->local_url; ?>?utm_source=wpplugin&utm_medium=welcome_page&utm_campaign=<?php echo $this->local; ?>" target="_blank">
@@ -93,6 +93,6 @@
     </a>
 </p>
 <p class="TP-deteiledIncome TP-deteiledIncomeR">
-    <?php _ex('tp_admin_page_wizard_tab_menu_content_paragraph_8',
-        '(We wish success to all your endeavors! Do the things you love and we will take care of the rest!)', TPOPlUGIN_TEXTDOMAIN); ?>
+    <?php _ex('We wish success to all your endeavors! Do the things you love and we will take care of the rest!',
+        'tp_admin_page_wizard_tab_menu_content_paragraph_8', TPOPlUGIN_TEXTDOMAIN); ?>
 </p>
