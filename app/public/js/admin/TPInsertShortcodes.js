33533dc0d2a3287f991be99404d842a4e97ce6cf
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2018-08-30 00:39:48 +0300

    add shortcode param power_by constructor widget field_power_by

--- app/public/js/admin/TPInsertShortcodes.js
+++ app/public/js/admin/TPInsertShortcodes.js
@@ -1105,7 +1105,7 @@ jQuery(function($) {
                     click: function() {
                         var origin, destination, width, height, direct, one_way, responsive, hotel_id, count, location,
                             cat, cat1, cat2, cat3, selected, zoom, typeHotelSelectView, limit, tp_subid,
-                            calendar_period, calendar_period_from, calendar_period_to;
+                            calendar_period, calendar_period_from, calendar_period_to, powered_by;
 
 
                         selected = doc.find('#select_widgets').val();
@@ -1143,6 +1143,13 @@ jQuery(function($) {
                         typeHotelSelectView = doc.find('#type_widget').val();
                         limit =  doc.find('#limit_widget').val();
                         tp_subid = doc.find('#tp_subid_widget').val();
+
+                        if(doc.find('#powered_by_widget').is(":checked")){
+                            powered_by = "powered_by=\"true\"";
+                        }else{
+                            powered_by = "powered_by=\"false\"";
+                        }
+
                         if(!validSubid(tp_subid)){
                             doc.find('#tp_subid_widget').addClass('constructorShortcodesError');
                         }else {
@@ -1201,11 +1208,15 @@ jQuery(function($) {
                                             responsive = "width="+doc.find('#responsive_width').val();
                                         }
 
+
+
+
+
                                         setShortcodes("[tp_calendar_widget origin=\""+origin+"\" destination=\""+destination+"\" "
                                             +direct+" "+one_way+" "+responsive+" subid=\""+tp_subid+"\"" +
                                             " period_day_from=\""+calendar_period_from+"\" " +
                                             " period_day_to=\""+calendar_period_to+"\"" +
-                                            " period=\""+calendar_period+"\"]",
+                                            " period=\""+calendar_period+"\" "+powered_by+"]",
                                             $(this));
                                     }
                                     break;
@@ -1240,7 +1251,7 @@ jQuery(function($) {
                                         }
                                         //width = doc.find('#hotel_size_widget_width').val();
                                         setShortcodes("[tp_hotel_widget hotel_id="+hotel_id+" "+responsive
-                                            +" subid=\""+tp_subid+"\"]",
+                                            +" subid=\""+tp_subid+"\" "+powered_by+"]",
                                             $(this));
                                     }
                                     break;
@@ -1259,7 +1270,7 @@ jQuery(function($) {
                                                 responsive = "width="+doc.find('#responsive_width').val();
                                             }
                                             setShortcodes("[tp_popular_routes_widget destination="+destination_r+"  "
-                                                +responsive+" subid=\""+tp_subid+"\"]",
+                                                +responsive+" subid=\""+tp_subid+"\" "+powered_by+"]",
                                                 $(this));
                                         }
                                     }else{
@@ -1274,7 +1285,7 @@ jQuery(function($) {
                                                 doc.find('#popular_routes_destination-'+i).addClass('constructorShortcodesError');
                                             }else{
                                                 count_s++;
-                                                shortcode += "<div class='TP-PopularRoutesWidget'>[tp_popular_routes_widget destination="+destination_r+" "+responsive+" subid=\""+tp_subid+"\"]</div>";
+                                                shortcode += "<div class='TP-PopularRoutesWidget'>[tp_popular_routes_widget destination="+destination_r+" "+responsive+" subid=\""+tp_subid+"\" "+powered_by+"]</div>";
                                             }
                                         }
                                         if(count == count_s){
@@ -1293,7 +1304,7 @@ jQuery(function($) {
 
                                         setShortcodes("[tp_hotel_selections_widget id="+hotel_id+" "
                                             +cat+" type="+typeHotelSelectView+" limit="+limit
-                                            +" subid=\""+tp_subid+"\"]",
+                                            +" subid=\""+tp_subid+"\" "+powered_by+"]",
                                             $(this));
                                     }
                                     break;
@@ -1340,7 +1351,7 @@ jQuery(function($) {
 
                                     //console.log(parrams);
 
-                                    setShortcodes("[tp_ducklett_widget "+parrams+" subid=\""+tp_subid+"\"]",
+                                    setShortcodes("[tp_ducklett_widget "+parrams+" subid=\""+tp_subid+"\" "+powered_by+"]",
                                         $(this));
                                     break;
                             }
