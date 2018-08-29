7ae78f3b2cdddb0bf93ea0f62c47c03a2f239d34
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2018-08-30 01:11:34 +0300

    add powered_by shortcode [tp_hotel_widget]

--- app/includes/controllers/site/widgets/TPHotelWidgetController.php
+++ app/includes/controllers/site/widgets/TPHotelWidgetController.php
@@ -24,6 +24,7 @@ class TPHotelWidgetController extends \app\includes\controllers\site\TPWigetsSho
             'width' => \app\includes\TPPlugin::$options['widgets'][$widgets]['width'],
             'subid' => '',
             'currency' => \app\includes\TPPlugin::$options['local']['currency'], //TPCurrencyUtils::TP_CURRENCY_USD,
+            'powered_by' => (isset(\app\includes\TPPlugin::$options['widgets'][$widgets]['powered_by']))? "true" : "false"
         );
         extract( wp_parse_args( $data, $defaults ), EXTR_SKIP );
         $width = (isset($responsive) && $responsive == 'true')? "?" : "?width={$width}px&";
@@ -36,7 +37,7 @@ class TPHotelWidgetController extends \app\includes\controllers\site\TPWigetsSho
             <div class="TPWidget TPHotelWidget">
             <script data-cfasync="false" async src="//www.travelpayouts.com/chansey/iframe.js'.$width.'&hotel_id='.$hotel_id
             .'&locale='.\app\includes\common\TPLang::getLang().'&host='.$white_label.'&marker='.$this->view->getMarker($widgets, $subid)
-            .'&currency='.mb_strtolower($currency).'" data-wpfc-render="false">
+            .'&currency='.mb_strtolower($currency).'&powered_by='.$powered_by.'" data-wpfc-render="false">
                    </script></div>';
         //%2Fsearch
         return $output;
